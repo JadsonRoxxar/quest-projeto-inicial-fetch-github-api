41fa65e7b5b0cdf1cbac79445d628de68580e8c1
JadsoRoxxar <jadsonrocha.jrda@gmail.com>
2024-04-02 16:36:06 -0300

    validações e remoção de erros

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,7 @@ import { screen } from "/src/scripts/objects/screen.js";
 
 document.getElementById("btn-search").addEventListener("click", () => {
     const userName = document.getElementById("input-search").value;
+    if (validateEmptyInput(userName)) return;
     getUserData(userName);
 });
 
@@ -15,49 +16,31 @@ document.getElementById("input-search").addEventListener("keyup", (e) => {
     const isKeyPressed = key === 13;
 
     if (isKeyPressed) {
+        if (validateEmptyInput(userName)) return;
         getUserData(userName);
     }
 });
 
+function validateEmptyInput(userName) {
+    if (userName.length === 0) {
+        alert("Preencha o campo com o nome do usuário do GitHub");
+        return true;
+    }
+}
+
 async function getUserData(userName) {
     const userResponse = await getUser(userName);
+
+    if (userResponse.message === "Not Found") {
+        screen.renderNotFound();
+        return;
+    }
     const repositoriesResponse = await getRepositories(userName);
+
     user.setInfo(userResponse);
     user.setRepositories(repositoriesResponse);
 
     screen.renderUser(user);
-
-    // getUser(userName).then((userData) => {
-    //     let userInfo = `<div class="info">
-    //         <img src= "${
-    //             userData.avatar_url
-    //         }" alt="Foto do perfil do usuário" />
-    //         <div class="data">
-    //             <h1>${userData.name ?? "Não possui nome cadastrado 😥"}</h1>
-    //             <p>${userData.bio ?? "Não possui bio cadastrada 😥"}</p>
-    //         </div>
-    //     </div>`;
-
-    //     document.querySelector(".profile-data").innerHTML = userInfo;
-    //
-    //   getUserRepositories(userName);
-    // });
-}
-
-function getUserRepositories(userName) {
-    getRepositories(userName).then((reposData) => {
-        let repositoriesItems = "";
-
-        reposData.forEach((repo) => {
-            repositoriesItems += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`;
-        });
-        document.querySelector(
-            ".profile-data"
-        ).innerHTML += `<div class="repositories section">
-                            <h2>Repositórios</h2>
-                            <ul>${repositoriesItems}</ul>
-                        </div>`;
-    });
 }
 
 /*
